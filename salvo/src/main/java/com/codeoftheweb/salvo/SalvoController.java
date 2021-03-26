ad749a29f7be9952ef980e1b96f4f4e4d4662b92
DESKTOP-GB\Gaston <gaston82@hotmail.com>
2021-03-26 09:29:14 -0300

    Fin de la primera entrega de "Implement Games"

--- salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -3,9 +3,13 @@ package com.codeoftheweb.salvo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 @RestController
@@ -23,9 +27,36 @@ public class SalvoController {
         return playerRepository.findAll();
     }
 
-    @RequestMapping("/games")
+    /* @RequestMapping("/games")
     public Set<Long> getGames() {
         return gameRepository.findAll().stream().map(Game::getId).collect(toSet());
+    }*/
+
+    @RequestMapping("/games")
+    public List<Map<String, Object>> getGames() {
+        return gameRepository.findAll().stream().map(this::makeGameDTO).collect(toList());
+    }
+
+    public Map<String, Object> makeGameDTO(Game game){
+        Map<String, Object> dto = new LinkedHashMap<String, Object>();
+        dto.put("id", game.getId());
+        dto.put("date", game.getDateTime());
+        dto.put("gamePlayer", game.getGamePlayers().stream().map(this::makeGamePlayerDTO).collect(toSet()));
+        return dto;
+    }
+
+    private Map<String, Object> makeGamePlayerDTO(GamePlayer gamePlayer){
+        Map<String, Object> dto = new LinkedHashMap<String, Object>();
+        dto.put("id", gamePlayer.getId());
+        dto.put("Player", makePlayerDTO(gamePlayer.getPlayer()));
+        return dto;
+    }
+
+    private Map<String, Object> makePlayerDTO(Player player){
+        Map<String, Object> dto = new LinkedHashMap<String, Object>();
+        dto.put("id", player.getId());
+        dto.put("email", player.getUserName());
+        return dto;
     }
 }
 
